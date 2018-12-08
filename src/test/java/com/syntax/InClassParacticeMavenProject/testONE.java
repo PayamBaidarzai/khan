11474079c47d5d2eb52b164c2917c8a05e15c274
PayamBaidarzai <payambidarzui143n@gmail.com>
2018-12-08 12:21:19 -0500

    add another syso

--- src/test/java/com/syntax/InClassParacticeMavenProject/testONE.java
+++ src/test/java/com/syntax/InClassParacticeMavenProject/testONE.java
@@ -2,4 +2,11 @@ package com.syntax.InClassParacticeMavenProject;
 
 public class testONE {
 
+	public void test() {
+		System.out.println("this is my new test");
+		
+		
+		System.out.println("this is my new test");
+		System.out.println("this is my new test");
+	}
 }
